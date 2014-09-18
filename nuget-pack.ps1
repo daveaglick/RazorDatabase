a05e4da013595ea4c9c008af873802ea265af607
Dave Glick <dglick@sipc.org>
2014-09-18 11:53:23 -0400

    More fun with paths.

--- nuget-pack.ps1
+++ nuget-pack.ps1
@@ -1,4 +1,4 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
 $version = [System.Reflection.Assembly]::LoadFile("$root\RazorDatabase\RazorDatabase\bin\Release\RazorDatabase.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
-& $root\RazorDatabase\.nuget\NuGet.exe pack $root\RazorDatabase\RazorDatabase.csproj -Version $versionStr
+& $root\RazorDatabase\.nuget\NuGet.exe pack $root\RazorDatabase\RazorDatabase\RazorDatabase.csproj -Version $versionStr
