d432ad4b23e44d1e015485d3331ad4275e74c13d
Dave Glick <dglick@sipc.org>
2014-09-18 11:36:32 -0400

    Tweaking build paths

--- nuget-pack.ps1
+++ nuget-pack.ps1
@@ -1,12 +1,12 @@
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
-$version = [System.Reflection.Assembly]::LoadFile("$root\RazorDatabase\bin\Release\RazorDatabase.dll").GetName().Version
+$version = [System.Reflection.Assembly]::LoadFile("$root\RazorDatabase\RazorDatabase\bin\Release\RazorDatabase.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
-$content = (Get-Content $root\RazorDatabase.nuspec) 
+$content = (Get-Content $root\RazorDatabase\RazorDatabase.nuspec) 
 $content = $content -replace '\$version\$',$versionStr
 
-$content | Out-File $root\RazorDatabase.compiled.nuspec
+$content | Out-File $root\RazorDatabase\RazorDatabase.compiled.nuspec
 
-& $root\.nuget\NuGet.exe pack $root\RazorDatabase.compiled.nuspec
+& $root\RazorDatabase\.nuget\NuGet.exe pack $root\RazorDatabase\RazorDatabase.compiled.nuspec
